cd03edd593068ceef17ebf4994e1eba52ff4694f
Ilya Lavrentyev <39114849+Lavrentyev-Ilya@users.noreply.github.com>
2026-04-01 18:49:14 +0300

    Remove ESLint ignoreDuringBuilds configuration
    Removed ESLint configuration to ignore build errors.

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ const nextConfig: NextConfig = {
   typescript: {
     // Игнорировать ошибки типов при сборке
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Можно также добавить это, чтобы билд не падал из-за правил оформления кода
-    ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
